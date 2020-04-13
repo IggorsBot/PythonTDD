1a3d64811ced7ac5792a5464d12cf3812a16a8e9
IggorsBot <IggorsBot@gmail.com>
2020-04-13 20:31:24 +0300

    remove unnecessary

--- functional_test.py
+++ functional_test.py
@@ -1,9 +1,5 @@
 from selenium import webdriver
-# from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-
-# binary = FirefoxBinary('/usr/lib/firefox')
-# browser = webdriver.Firefox(firefox_binary=binary, executable_path="/usr/local/bin/")
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
