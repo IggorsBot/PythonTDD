5da9240adeb10708d0e56788ac36386a1b73d7f2
IggorsBot <IggorsBot@gmail.com>
2020-04-15 19:35:04 +0300

    chapter 6

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,7 +3,10 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -25,15 +28,26 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(1)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
+        # self.browser.get('http://localhost:8000')
 
         self.insert_value('Buy peacock feathers')
         self.insert_value('Use peacock feathers to make a fly')
 
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         self.fail('Finish the test!')
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
